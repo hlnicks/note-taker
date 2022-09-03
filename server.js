49a61620bd5651423a47bf8db81bd0cc7b147669
hlnicks <hlnicks@icloud.com>
2022-09-03 12:34:25 -0400

    fixed a require and created function to connect to server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // requires
 const express = require("express");
 const fs = require("fs");
-const app = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Now listening to post ${PORT} `)
+})
 
