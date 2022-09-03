321f5734ec07ea9b2ead92b68ce00c361baeda10
hlnicks <hlnicks@icloud.com>
2022-09-03 12:47:11 -0400

    created newNote function and added pseudocode

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 // requires
+const { resolveSoa } = require("dns");
 const express = require("express");
 const fs = require("fs");
 const app = express();
 const path = require("path");
+const { ppid } = require("process");
 const PORT = process.env.PORT || 3001;
+const notes = require("./db/db.json");
+
+function createNote() {
+    // writes data
+    fs.writeFileSync(
+
+    );
+
+    // save data
+}
 
 // HTML routes
 
-// createNote function
-
 // GET route
+// app.get
 
 // POST
+// app.post
+
 
 app.listen(PORT, () => {
     console.log(`Now listening to post ${PORT} `)
