500adcbdaa4a70a43e4ec259938adff8ca5a9017
hlnicks <hlnicks@icloud.com>
2022-09-03 12:36:19 -0400

    added pseudocode

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 
+// HTML routes
+
+// createNote function
+
+// GET route
+
+// POST
+
 app.listen(PORT, () => {
     console.log(`Now listening to post ${PORT} `)
 })
